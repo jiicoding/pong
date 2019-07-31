db754c401e190cb1c193f0c99c058a91b9446bc4
wilkin <wilkin4@gmail.com>
2019-07-31 12:12:53 -0700

    add ball paddle, set window

--- src/Main.hx
+++ src/Main.hx
@@ -16,6 +16,12 @@ class Main extends Sprite
 
     var inited:Bool;
 
+    private var platform1:Platform;
+
+    private var platform2:Platform;
+
+    private var ball:Ball;
+
 
 
     /* ENTRY POINT */
@@ -44,7 +50,33 @@ class Main extends Sprite
 
 
 
-        // code
+        platform1 = new Platform();
+
+        platform1.x = 5;
+
+        platform1.y = 200;
+
+        this.addChild(platform1);
+
+
+
+        platform2 = new Platform();
+
+        platform2.x = 480;
+
+        platform2.y = 200;
+
+        this.addChild(platform2);
+
+
+
+        ball = new Ball();
+
+        ball.x = 250;
+
+        ball.y = 250;
+
+        this.addChild(ball);
 
     }
 
