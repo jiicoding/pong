6a63a61ccdef6aca4f021def2e2c14ecedb74d10
wilkin <wilkin4@gmail.com>
2019-07-31 12:39:30 -0700

    game logic

--- src/Main.hx
+++ src/Main.hx
@@ -1,16 +1,46 @@
 package ;
 
 
+
 import openfl.display.Sprite;
 
 import openfl.events.Event;
 
 import openfl.Lib;
 
+import openfl.events.KeyboardEvent;
+
+import openfl.geom.Point;
+
+import openfl.text.TextField;
+
+import openfl.text.TextFormat;
+
+import openfl.text.TextFormatAlign;
+
+
+enum GameState {
+
+    Paused;
+
+    Playing;
+
+}
+
+
+enum Player {
+
+    Human;
+
+    AI;
+
+}
+
 
 class Main extends Sprite {
 
     var inited:Bool;
+
 
     private var platform1:Platform;
 
@@ -18,12 +48,33 @@ class Main extends Sprite {
 
     private var ball:Ball;
 
+    private var scorePlayer:Int;
+
+    private var scoreAI:Int;
+
+    private var scoreField:TextField;
+
+    private var messageField:TextField;
+
+    private var currentGameState:GameState;
+
+    private var arrowKeyUp:Bool;
+
+    private var arrowKeyDown:Bool;
+
+    private var platformSpeed:Int;
+
+    private var ballMovement:Point;
+
+    private var ballSpeed:Int;
+
+
 
     /* ENTRY POINT */
 
 
     function resize(e) {
-        trace('stage resized');
+
         if (!inited) init();
 
         // else (resize or orientation change)
@@ -38,6 +89,7 @@ class Main extends Sprite {
         inited = true;
 
 
+
         platform1 = new Platform();
 
         platform1.x = 5;
@@ -45,6 +97,7 @@ class Main extends Sprite {
         platform1.y = 200;
 
         this.addChild(platform1);
+
 
 
         platform2 = new Platform();
@@ -56,6 +109,7 @@ class Main extends Sprite {
         this.addChild(platform2);
 
 
+
         ball = new Ball();
 
         ball.x = 250;
@@ -64,7 +118,260 @@ class Main extends Sprite {
 
         this.addChild(ball);
 
+
+        var scoreFormat:TextFormat = new TextFormat("Verdana", 24, 0xbbbbbb, true);
+
+        scoreFormat.align = cast TextFormatAlign.CENTER;
+
+
+        scoreField = new TextField();
+
+        addChild(scoreField);
+
+        scoreField.width = 500;
+
+        scoreField.y = 30;
+
+        scoreField.defaultTextFormat = scoreFormat;
+
+        scoreField.selectable = false;
+
+
+        var messageFormat:TextFormat = new TextFormat("Verdana", 18, 0xbbbbbb, true);
+
+        messageFormat.align = cast TextFormatAlign.CENTER;
+
+
+        messageField = new TextField();
+
+        addChild(messageField);
+
+        messageField.width = 500;
+
+        messageField.y = 450;
+
+        messageField.defaultTextFormat = messageFormat;
+
+        messageField.selectable = false;
+
+        messageField.text = cast "Press SPACE to start\nUse ARROW KEYS to move your platform";
+
+
+        scorePlayer = 0;
+
+        scoreAI = 0;
+
+        arrowKeyUp = false;
+
+        arrowKeyDown = false;
+
+        platformSpeed = 7;
+
+        ballSpeed = 7;
+
+        ballMovement = new Point(0, 0);
+
+        setGameState(Paused);
+
+
+        stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
+
+        stage.addEventListener(KeyboardEvent.KEY_UP, keyUp);
+
+        this.addEventListener(Event.ENTER_FRAME, everyFrame);
+
     }
+
+
+    private function setGameState(state:GameState):Void {
+
+        currentGameState = state;
+
+        updateScore();
+
+        if (state == Paused) {
+
+            messageField.alpha = 1;
+
+        } else {
+
+            messageField.alpha = 0;
+
+            platform1.y = 200;
+
+            platform2.y = 200;
+
+            ball.x = 250;
+
+            ball.y = 250;
+
+            var direction:Int = (Math.random() > .5) ? (1) : ( -1);
+
+            var randomAngle:Float = (Math.random() * Math.PI / 2) - 45;
+
+            ballMovement.x = direction * Math.cos(randomAngle) * ballSpeed;
+
+            ballMovement.y = Math.sin(randomAngle) * ballSpeed;
+
+        }
+
+    }
+
+
+    private function keyDown(event:KeyboardEvent):Void {
+
+        if (currentGameState == Paused && event.keyCode == 32) { // Space
+
+            setGameState(Playing);
+
+        } else if (event.keyCode == 38) { // Up
+
+            arrowKeyUp = true;
+
+        } else if (event.keyCode == 40) { // Down
+
+            arrowKeyDown = true;
+
+        }
+
+    }
+
+
+    private function keyUp(event:KeyboardEvent):Void {
+        var c = String.fromCharCode(event.charCode);
+        trace("key up", c);
+        if (event.keyCode == 38) { // Up
+
+            arrowKeyUp = false;
+
+        } else if (event.keyCode == 40) { // Down
+
+            arrowKeyDown = false;
+
+        }
+
+    }
+
+
+    private function everyFrame(event:Event):Void {
+
+        if (currentGameState == Playing) {
+
+            // player platform movement
+
+            if (arrowKeyUp) {
+
+                platform1.y -= platformSpeed;
+
+            }
+
+            if (arrowKeyDown) {
+
+                platform1.y += platformSpeed;
+
+            }
+
+            // AI platform movement
+
+            if (ball.x > 300 && ball.y > platform2.y + 70) {
+
+                platform2.y += platformSpeed;
+
+            }
+
+            if (ball.x > 300 && ball.y < platform2.y + 30) {
+
+                platform2.y -= platformSpeed;
+
+            }
+
+            // player platform limits
+
+            if (platform1.y < 5) platform1.y = 5;
+
+            if (platform1.y > 395) platform1.y = 395;
+
+            // AI platform limits
+
+            if (platform2.y < 5) platform2.y = 5;
+
+            if (platform2.y > 395) platform2.y = 395;
+
+            // ball movement
+
+            ball.x += ballMovement.x;
+
+            ball.y += ballMovement.y;
+
+            // ball platform bounce
+
+            if (ballMovement.x < 0 && ball.x < 30 && ball.y >= platform1.y && ball.y <= platform1.y + 100) {
+
+                bounceBall();
+
+                ball.x = 30;
+
+            }
+
+            if (ballMovement.x > 0 && ball.x > 470 && ball.y >= platform2.y && ball.y <= platform2.y + 100) {
+
+                bounceBall();
+
+                ball.x = 470;
+
+            }
+
+            // ball edge bounce
+
+            if (ball.y < 5 || ball.y > 495) ballMovement.y *= -1;
+
+            // ball goal
+
+            if (ball.x < 5) winGame(AI);
+
+            if (ball.x > 495) winGame(Human);
+
+        }
+
+    }
+
+
+    private function bounceBall():Void {
+
+        var direction:Int = (ballMovement.x > 0) ? ( -1) : (1);
+
+        var randomAngle:Float = (Math.random() * Math.PI / 2) - 45;
+
+        ballMovement.x = direction * Math.cos(randomAngle) * ballSpeed;
+
+        ballMovement.y = Math.sin(randomAngle) * ballSpeed;
+
+    }
+
+
+    private function winGame(player:Player):Void {
+
+        if (player == Human) {
+
+            scorePlayer++;
+
+        } else {
+
+            scoreAI++;
+
+        }
+
+        setGameState(Paused);
+
+    }
+
+
+    private function updateScore():Void {
+
+        scoreField.text = cast scorePlayer + ":" + scoreAI;
+
+    }
+
 
 
     /* SETUP */
@@ -113,4 +420,3 @@ class Main extends Sprite {
     }
 
 }
-
